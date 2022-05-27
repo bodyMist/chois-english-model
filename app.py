6ae721c4b6871b7e78eb15bccd8abcb65a18f3c7
bodymist <sky834459@gmail.com>
2022-05-27 15:59:35 +0900

    add tokenizer function to create blank on caption

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
 from flask_cors import CORS
+import json 
 
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -79,115 +80,136 @@ class WordScoring(Resource):
     return res
 
 
-import torch
-from torchvision import transforms
-import numpy as np
-from PIL import Image
-from fairseq import utils, tasks
-from fairseq import checkpoint_utils
-from utils.eval_utils import eval_step
-from tasks.mm_tasks.caption import CaptionTask
-from models.ofa import OFAModel
+# import torch
+# from torchvision import transforms
+# import numpy as np
+# from PIL import Image
+# from fairseq import utils, tasks
+# from fairseq import checkpoint_utils
+# from utils.eval_utils import eval_step
+# from tasks.mm_tasks.caption import CaptionTask
+# from models.ofa import OFAModel
 
-tasks.register_task('caption', CaptionTask)
-# turn on cuda if GPU is available
-use_cuda = torch.cuda.is_available()
-# use fp16 only when GPU is available
-use_fp16 = False
+# tasks.register_task('caption', CaptionTask)
+# # turn on cuda if GPU is available
+# use_cuda = torch.cuda.is_available()
+# # use fp16 only when GPU is available
+# use_fp16 = False
 
-# Load pretrained ckpt & config
-overrides = {"bpe_dir":"utils/BPE", "eval_cider":False, "beam":5, "max_len_b":16, "no_repeat_ngram_size":3, "seed":7}
-models, cfg, task = checkpoint_utils.load_model_ensemble_and_task(
-        utils.split_paths('checkpoints/caption.pt'),
-        arg_overrides = overrides
-    )
+# # Load pretrained ckpt & config
+# overrides = {"bpe_dir":"utils/BPE", "eval_cider":False, "beam":5, "max_len_b":16, "no_repeat_ngram_size":3, "seed":7}
+# models, cfg, task = checkpoint_utils.load_model_ensemble_and_task(
+#         utils.split_paths('checkpoints/caption.pt'),
+#         arg_overrides = overrides
+#     )
 
-# Move models to GPU
-for model in models:
-    model.eval()
-    if use_fp16:
-        model.half()
-    if use_cuda and not cfg.distributed_training.pipeline_model_parallel:
-        model.cuda()
-    model.prepare_for_inference_(cfg)
+# # Move models to GPU
+# for model in models:
+#     model.eval()
+#     if use_fp16:
+#         model.half()
+#     if use_cuda and not cfg.distributed_training.pipeline_model_parallel:
+#         model.cuda()
+#     model.prepare_for_inference_(cfg)
 
-# Initialize generator
-generator = task.build_generator(models, cfg.generation)
+# # Initialize generator
+# generator = task.build_generator(models, cfg.generation)
 
-mean = [0.5, 0.5, 0.5]
-std = [0.5, 0.5, 0.5]
+# mean = [0.5, 0.5, 0.5]
+# std = [0.5, 0.5, 0.5]
 
-patch_resize_transform = transforms.Compose([
-    lambda image: image.convert("RGB"),
-    transforms.Resize((cfg.task.patch_image_size, cfg.task.patch_image_size), interpolation=Image.BICUBIC),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=mean, std=std),
-])
+# patch_resize_transform = transforms.Compose([
+#     lambda image: image.convert("RGB"),
+#     transforms.Resize((cfg.task.patch_image_size, cfg.task.patch_image_size), interpolation=Image.BICUBIC),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=mean, std=std),
+# ])
 
-# Text preprocess
-bos_item = torch.LongTensor([task.src_dict.bos()])
-eos_item = torch.LongTensor([task.src_dict.eos()])
-pad_idx = task.src_dict.pad()
+# # Text preprocess
+# bos_item = torch.LongTensor([task.src_dict.bos()])
+# eos_item = torch.LongTensor([task.src_dict.eos()])
+# pad_idx = task.src_dict.pad()
 
-def encode_text(text, length=None, append_bos=False, append_eos=False):
-    s = task.tgt_dict.encode_line(
-        line=task.bpe.encode(text),
-        add_if_not_exist=False,
-        append_eos=False
-    ).long()
-    if length is not None:
-        s = s[:length]
-    if append_bos:
-        s = torch.cat([bos_item, s])
-    if append_eos:
-        s = torch.cat([s, eos_item])
-    return s
-
-
-# Construct input for caption task
-def construct_sample(image: Image):
-    patch_image = patch_resize_transform(image).unsqueeze(0)
-    patch_mask = torch.tensor([True])
-    src_text = encode_text(" what does the image describe?", append_bos=True, append_eos=True).unsqueeze(0)
-    src_length = torch.LongTensor([s.ne(pad_idx).long().sum() for s in src_text])
-    sample = {
-        "id":np.array(['42']),
-        "net_input": {
-            "src_tokens": src_text,
-            "src_lengths": src_length,
-            "patch_images": patch_image,
-            "patch_masks": patch_mask
-        }
-    }
-    return sample
+# def encode_text(text, length=None, append_bos=False, append_eos=False):
+#     s = task.tgt_dict.encode_line(
+#         line=task.bpe.encode(text),
+#         add_if_not_exist=False,
+#         append_eos=False
+#     ).long()
+#     if length is not None:
+#         s = s[:length]
+#     if append_bos:
+#         s = torch.cat([bos_item, s])
+#     if append_eos:
+#         s = torch.cat([s, eos_item])
+#     return s
 
 
-# Function to turn FP32 to FP16
-def apply_half(t):
-    if t.dtype is torch.float32:
-        return t.to(dtype=torch.half)
-    return t
+# # Construct input for caption task
+# def construct_sample(image: Image):
+#     patch_image = patch_resize_transform(image).unsqueeze(0)
+#     patch_mask = torch.tensor([True])
+#     src_text = encode_text(" what does the image describe?", append_bos=True, append_eos=True).unsqueeze(0)
+#     src_length = torch.LongTensor([s.ne(pad_idx).long().sum() for s in src_text])
+#     sample = {
+#         "id":np.array(['42']),
+#         "net_input": {
+#             "src_tokens": src_text,
+#             "src_lengths": src_length,
+#             "patch_images": patch_image,
+#             "patch_masks": patch_mask
+#         }
+#     }
+#     return sample
 
 
-def image_caption(Image):
-    sample = construct_sample(Image)
-    sample = utils.move_to_cuda(sample) if use_cuda else sample
-    sample = utils.apply_to_sample(apply_half, sample) if use_fp16 else sample
-    with torch.no_grad():
-        result, scores = eval_step(task, generator, models, sample)
-    print(result)
-    return result[0]['caption']
+# # Function to turn FP32 to FP16
+# def apply_half(t):
+#     if t.dtype is torch.float32:
+#         return t.to(dtype=torch.half)
+#     return t
+
+
+# def image_caption(Image):
+#     sample = construct_sample(Image)
+#     sample = utils.move_to_cuda(sample) if use_cuda else sample
+#     sample = utils.apply_to_sample(apply_half, sample) if use_fp16 else sample
+#     with torch.no_grad():
+#         result, scores = eval_step(task, generator, models, sample)
+#     print(result)
+#     return result[0]['caption']
+
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+import random
+
+# delete stop_words from caption
+# tokenize caption to create blank
+def tokenizeCaption(caption):
+  stop_words = set(stopwords.words('english')) 
+  word_tokens = word_tokenize(caption)
+
+  result = []
+  for word in word_tokens: 
+      if word not in stop_words: 
+          result.append(word)
+
+  return random.choice(result),
+  
 
 # 이미지 캡셔닝 요청 API
 @api.route('/caption', methods=['POST'])
 class Caption(Resource):
   def post(self):
     try:
-      imageFile = request.files['file']
-      image = Image.open(imageFile)
-      caption = image_caption(image)
-      response = dict(zip("caption", caption))
-      return jsonify(response)
+      # imageFile = request.files['file']
+      # image = Image.open(imageFile)
+      # caption = image_caption(image)
+      blank = tokenizeCaption(caption)
+      # response = dict(zip("caption", caption))
+      # return jsonify(response)
+      return json.dumps({'success':True}, {'caption': caption}, {'blank':blank}), 200, {'ContentType':'application/json'} 
     except Exception as e:
       print(e)
 
